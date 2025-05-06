4ac063fd5440c727e3b2e280a54fbf47ee564860
rafanr131 <rafanr131@gmail.com>
2025-05-05 21:38:21 -0300

    add all petapp project

--- obj/Debug/net8.0/VSstudy2.AssemblyInfo.cs
+++ obj/Debug/net8.0/VSstudy2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VSstudy2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c992ab141b52e4ec34164655adcec2be00d0a757")]
 [assembly: System.Reflection.AssemblyProductAttribute("VSstudy2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VSstudy2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
